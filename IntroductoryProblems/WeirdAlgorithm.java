467fc5bc29d5ed414ab61ea0f989dc5399e5b3e5
Sivadhas <sivadhas007@gmail.com>
2024-01-19 20:19:57 +0530

    1.1 basic loop

---
+++ IntroductoryProblems/WeirdAlgorithm.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class WeirdAlgorithm{
+  public static void main(String[] args) {
+    Scanner in=new Scanner(System.in);
+    long n=in.nextLong();
+    while(n>1){
+      System.out.print(n+" ");
+      if(n%2==0) n/=2;
+      else n=n*3+1;
+    }
+    System.out.println(1);
+  }
+
+}
